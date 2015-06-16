6dc9089c259ab623cc160c7281c6ed1170a8b0b6
unknown <mohd.nazim@ctnl1134keshav.ctech.com>
2015-06-16 22:09:02 +0530

    Added feeds script

---
+++ Seleniumtext.bat
@@ -0,0 +1,3 @@
+java -jar D:\SeleniumTest\selenium-server-standalone-2.45.0.jar -htmlSuite "*firefox" "http://jsoneditoronline.org/" "D:\SeleniumTest\SeleniumSuite.HTML" "D:\SeleniumTest\seleniumtestresult.html"
+
+D:\SeleniumTest\seleniumtestresult.html
